41919935f4a9d46f8640eaa0f9d4c0483492fbc6
YoonjuSeo <yoonjuseo08@gmail.com>
2019-02-10 23:22:14 +0900

    9 Event 190210

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ new Vue({
         title: 'Becoming a Vue master',
         name: 'Yoonju',
         url: 'https://www.youtube.com',
-        classes: ['one','two']
+        classes: ['one','two'],
+        wage : 10
     },
     methods: {
         greet(time){
             this.title
             return `Hello and good ${time}, ${this.name}!`
+        },
+        changeWage(amount){
+            this.wage += amount
         }
     }
 })
