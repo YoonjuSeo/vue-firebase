8d2287ef0860f4a0065750716544189cbe05a2da
YoonjuSeo <yoonjuseo08@gmail.com>
2019-02-12 16:53:27 +0900

    15 looping with v-for  190212

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ new Vue({
             y: 0
         },
         showName : true,
-        showAge : true
+        showAge : true,
+        items: ['Mushroom', 'Green Shells','Red Shell','Banana','Star'],
+        ninjas : [
+            {name : 'Crystal', age: 25, belt: 'Black'},
+            {name : 'Ryu', age: 30, belt: 'Brown'},
+            {name : 'Ken', age: 35, belt: 'Orange'}
+        ]
+    
     },
     methods: {
         greet(time){
