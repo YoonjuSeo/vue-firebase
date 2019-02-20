4cfa16d244fd5602f4d0e7bf03efd80dfaf1bc6b
bobo <bobocolfax@gmail.com>
2019-02-20 20:48:46 +0900

    28 Setting up Routes 190220

--- routing/src/router/index.js
+++ routing/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
     }
   ]
 })
