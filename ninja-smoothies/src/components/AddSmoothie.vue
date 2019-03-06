8e05966c4e7b3ebc6299cb304c8d8d87bc84802a
yoonju seo <yoonjuseo08@gmail.com>
2019-03-06 17:07:11 +0900

    49 outputting ingredients 190306

--- ninja-smoothies/src/components/AddSmoothie.vue
+++ ninja-smoothies/src/components/AddSmoothie.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class = "add-smoothie container">
+    <div class = "add-smoothie container z-depth-1">
         <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
         <form @submit.prevent="AddSmoothie">
             <div class="field title">
                 <label for="title">Smoothie Title:</label>
                 <input type="text" name="title" v-model="title">
             </div>
+            <div v-for="(ing, index) in ingredients" class = "field ingredient" :key="index">
+                <label for="ingredient">Ingredient:</label>
+                <input type="text" name="ingredient" v-model="ingredients[index]">
+            </div>
             <div class="field add-ingredient">
-                <label for="add-ingredient">Add an ingredient</label>
+                <label for="add-ingredient">Add an ingredient (press tab to add):</label>
                 <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
             </div>
             <div class="field center-align">
@@ -31,7 +35,7 @@ export default {
     },
     methods:{
         AddSmoothie(){
-            console.log(this.title)
+            console.log(this.title, this.ingredients)
         },
         addIng(){
             if(this.another){
